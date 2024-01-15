13d0619b9cccd23e2d5368bb7efd0a75f10c50df
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-01-15 16:47:07 +0000

    Create nav menu using mui popup state

--- app/components/components/dropdown.tsx
+++ app/components/components/dropdown.tsx
@@ -13,9 +13,17 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Open_Sans } from "next/font/google";
 import { opensans } from "@/app/layout";
+import HoverMenu from 'material-ui-popup-state/HoverMenu'
+import {
+  usePopupState,
+  bindHover,
+  bindFocus,
+  bindMenu,
+} from 'material-ui-popup-state/hooks'
+import theme from "@/app/styles/theme";
 
 const StyledMenu = styled((props: MenuProps) => (
-    <Menu
+    <HoverMenu
       elevation={0}
       anchorOrigin={{
         vertical: 'bottom',
@@ -58,27 +66,21 @@ const StyledMenu = styled((props: MenuProps) => (
 
 export default function DropDown({name, link, dropdown}: LinkType) {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const popupState = usePopupState({
+      variant: 'popover',
+      popupId: 'demoMenu',
+    })
   const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  
 
 
   return (
     <div>
       <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
         variant="contained"
+        {...bindHover(popupState)}
+        {...bindFocus(popupState)}
         disableElevation
-        onClick={handleClick}
-        onMouseOver={handleClick}
-        // onMouseLeave={handleClose}
         sx={{
             borderRadius: "9999px",
             textTransform: 'none',
@@ -90,8 +92,8 @@ export default function DropDown({name, link, dropdown}: LinkType) {
           
 
             '&.MuiButton-root': {
-              backgroundColor: open ? "#C296C7" : "#FFFBF3",
-              color: open ? "#FFFBF3" : "#6B5539",
+              backgroundColor: popupState.isOpen ? "#C296C7" : "#FFFBF3",
+              color: popupState.isOpen ? "#FFFBF3" : "#6B5539",
               '&:hover': {
                 color: "#FFFBF3",
                 backgroundColor: "#C296C7"
@@ -108,34 +110,58 @@ export default function DropDown({name, link, dropdown}: LinkType) {
       >
         options
       </Button>
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
+      <HoverMenu
+        {...bindMenu(popupState)}
+        anchorOrigin={{ vertical: 40, horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+        sx={{
+          '& .MuiPaper-root': {
+            // borderRadius: 4,
+            // marginTop: theme.spacing(3),
+            minWidth: 180,
+            color:
+              theme.palette.primary.contrastText,
+              backgroundColor: theme.palette.primary.main,
+            boxShadow:
+              'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+            '& .MuiMenu-list': {
+              padding: '4px 0',
+            },
+            '& .MuiMenuItem-root': {
+              '& .MuiSvgIcon-root': {
+                fontSize: 12,
+                color: theme.palette.primary.contrastText,
+                marginRight: theme.spacing(1.5),
+              },
+              '&:active': {
+                backgroundColor: alpha(
+                  theme.palette.primary.main,
+                  theme.palette.action.selectedOpacity,
+                ),
+              },
+            },
+          },
         }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
       >
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={popupState.close} disableRipple>
           <EditIcon />
           Edit
         </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={popupState.close} disableRipple>
           <FileCopyIcon />
           Duplicate
         </MenuItem>
         {/* <Divider sx={{ my: 0.5, color: "#FFFBF3" }} /> */}
         <div className="w-full h-[1px] bg-[var(--trans-cream)] my-1"></div>
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={popupState.close} disableRipple>
           <ArchiveIcon />
           Archive
         </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={popupState.close} disableRipple>
           <MoreHorizIcon />
           More
         </MenuItem>
-      </StyledMenu>
+      </HoverMenu>
     </div>
   );
 
