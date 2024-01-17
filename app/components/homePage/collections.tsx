1691c573d70ab81d922d989a7fea7b051d4db688
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-01-17 13:25:33 +0000

    Add paisleys to collection section

--- app/components/homePage/collections.tsx
+++ app/components/homePage/collections.tsx
@@ -3,7 +3,7 @@ import { BraceletCategory, CustomCategory, EarringCategory, NecklaceCategory, Pi
 
 export default function Collections() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen h-auto gap-y-[70px] py-32">
+    <div className="flex flex-col items-center justify-center min-h-screen h-auto gap-y-[70px] w-screen bg-cover py-32 bg-[url('https://raw.githubusercontent.com/reannab16/readmeimgs/03f4294c26db6520341bb3a4678abcb40d637c50/paisley-background.svg')]">
       <div className="text-[var(--maybe-text-colour)] text-3xl relative">
         COLLECTIONS{" "}
         <PinkPaisleyRight className="absolute -top-6 -right-[3.75rem] h-[63px]" />
@@ -14,7 +14,7 @@ export default function Collections() {
           return (
             <div className="border-[1px] h-[313px] border-[var(--dark-purple)] border-solid rounded-xl">
               {cate.img}
-              <div className="w-full flex items-center justify-center h-[42px] text-sm"><span className="text-[var(--maybe-text-colour)]">{cate.name}</span>&nbsp;<span className="text-[var(--trans-maybe-text-colour)]">{cate.quantity ? `(${cate.quantity})`: ""}</span></div>
+              <div className="w-full flex items-center justify-center h-[41px] rounded-b-xl text-sm bg-[var(--cream)]"><span className="text-[var(--maybe-text-colour)]">{cate.name}</span>&nbsp;<span className="text-[var(--trans-maybe-text-colour)]">{cate.quantity ? `(${cate.quantity})`: ""}</span></div>
             </div>
           );
         })}
