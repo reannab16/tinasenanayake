95002524f8d0905491f451a238f3799987c0a300
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-02-13 04:15:26 +0000

    Add category page temporary ui

--- app/shop/[category]/page.tsx
+++ app/shop/[category]/page.tsx
@@ -1,16 +1,22 @@
 import { products } from "@/app/utils/products";
 import React from "react";
+
 interface IPrams {
   category?: string;
 }
 
 export default function CategoryPage({ params }: { params: IPrams }) {
   return (
-    <div>
-      {params.category?.replaceAll("-", " ")}
+    <div className="flex flex-col text-[var(--maybe-text-colour)] items-start container w-screen px-5">
+        <div>shop | {params.category}</div>
+        <div className="italic text-3xl font-light pt-10 pb-8">{params.category?.replaceAll("-", " ")}</div>
+        <div className="flex gap-x-5 mb-14">
+            <div className="px-4 py-1 w-36 bg-[var(--light-purple)] rounded-full text-[var(--cream)] text-sm">all</div>
+            <div className="px-4 py-1 w-40 bg-[var(--light-purple)] rounded-full text-[var(--cream)] text-sm">Date, new to old</div>
+        </div>
+      
       <div className="">
         {products.map((item) => {
-          console.log(item.name, item.category[0][0] === params.category);
 
           return (
             item.category[0][0] === params.category && (
